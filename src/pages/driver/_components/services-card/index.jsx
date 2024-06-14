a8a27d18fe937655d6c0bf6636302a931b1c330c
encodedevv <timothyonyea@gmail.com>
2024-06-14 17:28:55 +0100

    first section, driver page

---
+++ src/pages/driver/_components/services-card/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "../../../../components";
+
+const ServicesCard = () => {
+  return (
+    <section className="container px-0 overflow-hidden mb-32 md:mb-40">
+      <div className="container container-md mx-auto">
+        <div className="flex flex-col gap-4 w-full">
+          <span className="text-5xl !leading-[1.3] text-[#131a53] font-semibold">
+            Benefits of becoming a Loadin driver.
+          </span>
+          <span className="text-lightgray w-1/2 text-lg">
+            Consectetur optio perferendis dignissimos animi dolor, sapiente,
+            molestiae voluptates corrupti harum fugit quam consequuntur
+            molestiae voluptates corrupti harum fugit quam consequuntur
+          </span>
+
+          <Button
+            children="Sign up"
+            border="none"
+            borderRadius="20px"
+            backgroundColor="#131a53"
+            textColor="#fff"
+            className="w-fit font-medium transition-all ease-in hover:scale-105 text-xs md:text-base"
+            padding={"10px 20px"}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ServicesCard;
