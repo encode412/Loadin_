1d5c5156c01615a2f7637cfcbe9e37d7b4a072e7
encodedevv <timothyonyea@gmail.com>
2024-06-16 18:57:54 +0100

    stars component

--- src/constants/index.js
+++ src/constants/index.js
@@ -107,3 +107,84 @@ export const Features = [
     imageUrl: datepicker,
   },
 ];
+
+export const recommendedDrivers = [
+  {
+    id: 0,
+    name: "Alabi Samuel",
+    profile_image: "/dashboard/profile1.png",
+    vehicle_image: "",
+    vehicle_type: "Pickup Truck",
+    vehicle_capacity: "50kg",
+    vehicle_description:
+      "My pickup truck is a versatile and reliable vehicle designed for transporting heavy loads. It's a go-to option for various cargo transportation needs",
+    rating: "4.0",
+    count: 4,
+    reviews: [
+      {
+        id: 0,
+        content:
+          "Absolutely impressed with the driver's professionalism and strength! He handled my goods with ease and delivered it right on time. Highly recommend his service",
+          reviewer: "Kehinde Poopola"
+      },
+
+      {
+        id: 1,
+        content:
+          "Hiring Mr Samuel was a game-changer for my business. He transported my heavy equipment with utmost care. His experience and expertise in moving heavy loads are unmatched",
+          reviewer: "Ajibola Francis"
+      },
+      {
+        id: 2,
+        content:
+          "I couldn't have asked for a better driver to handle my heavy cargo. He was punctual, reliable, and incredibly skilled.",
+          reviewer: "Sophia Adetunji"
+      },
+    ],
+    experience: 5,
+    achievement: "Recognized as the 'Efficient Mover of May 2024' for successfully transporting over 5 tons of weight without any incidents. This driver's exceptional strength, precision, and commitment to safety make him a top choice for handling even the most challenging heavy load moves.",
+    minutes_away: 5,
+    availability: ['Mon, Tues, Thurs, Sun'],
+    certified: true,
+    estimated_price: 8000,
+  },
+  {
+    id: 1,
+    name: "Tunde Ibrahim",
+    profile_image: "/dashboard/profile1.png",
+    vehicle_image: "",
+    vehicle_type: "Pickup Truck",
+    vehicle_capacity: "50kg",
+    vehicle_description:
+      "My pickup truck is a versatile and reliable vehicle designed for transporting heavy loads. It's a go-to option for various cargo transportation needs",
+    rating: "4.0",
+    count: 4,
+    reviews: [
+      {
+        id: 0,
+        content:
+          "Absolutely impressed with the driver's professionalism and strength! He handled my goods with ease and delivered it right on time. Highly recommend his service",
+          reviewer: "Kehinde Poopola"
+      },
+
+      {
+        id: 1,
+        content:
+          "Hiring Mr Samuel was a game-changer for my business. He transported my heavy equipment with utmost care. His experience and expertise in moving heavy loads are unmatched",
+          reviewer: "Ajibola Francis"
+      },
+      {
+        id: 2,
+        content:
+          "I couldn't have asked for a better driver to handle my heavy cargo. He was punctual, reliable, and incredibly skilled.",
+          reviewer: "Sophia Adetunji"
+      },
+    ],
+    experience: 5,
+    achievement: "Recognized as the 'Efficient Mover of May 2024' for successfully transporting over 5 tons of weight without any incidents. This driver's exceptional strength, precision, and commitment to safety make him a top choice for handling even the most challenging heavy load moves.",
+    minutes_away: 5,
+    availability: ['Mon, Tues, Thurs, Sun'],
+    certified: true,
+    estimated_price: 8000,
+  },
+];
