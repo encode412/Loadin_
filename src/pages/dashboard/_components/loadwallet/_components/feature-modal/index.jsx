34b2044fbe0264896b93d420fb944012c4d6b988
encodedevv <timothyonyea@gmail.com>
2024-06-19 06:12:50 +0100

    update to content

--- src/pages/dashboard/_components/loadwallet/_components/feature-modal/index.jsx
+++ src/pages/dashboard/_components/loadwallet/_components/feature-modal/index.jsx
@@ -7,12 +7,11 @@ const FeatureModal = () => {
         <span className="text-primary text-3xl font-bold">How to use LoadWallet</span>
         <div className="flex flex-col px-6 pt-6">
             <ul className="text-base flex flex-col gap-y-3 font-medium">
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>Access to best prices</li>
-                <li style={{listStyle: 'disc'}}>Access to best prices</li>
+                <li style={{listStyle: 'disc'}}> Follow the instructions to set up your LoadWallet account</li>
+                <li style={{listStyle: 'disc'}}>As a driver, when it's time for payment, access the LoadWallet feature. Simply request the amount required for moving the load directly on the platform. Provide all the necessary details, such as the load information and the payment amount.</li>
+                <li style={{listStyle: 'disc'}}>Customers will receive the payment request and can conveniently review it within the LoadConnect platform. They can verify the details of the payment request, ensuring accuracy and alignment with the agreed terms. With a simple click, they can approve the payment.</li>
+                <li style={{listStyle: 'disc'}}>Once the payment request is approved, the funds will be transferred from the Customer's LoadWallet account to the driver's LoadWallet account. This process ensures a seamless and efficient transaction, avoiding the delays associated with traditional transfers.</li>
+           
             </ul>
         </div>
       </div>
--- src/pages/dashboard/_components/post-bid/_components/feature-modal/index.jsx
+++ src/pages/dashboard/_components/post-bid/_components/feature-modal/index.jsx
@@ -6,13 +6,13 @@ const FeatureModal = () => {
       <div className="flex px-6 py-8 flex-col">
         <span className="text-primary text-3xl font-bold">How to use LoadConnect</span>
         <div className="flex flex-col px-6 pt-6">
-            <ul className="text-base flex flex-col gap-y-3 font-medium">
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>Access to best prices</li>
-                <li style={{listStyle: 'disc'}}>Access to best prices</li>
+            <ul className="text-base flex flex-col gap-y-4 font-medium">
+                <li style={{listStyle: 'disc'}}>Look for the "Post a Load" on the LoadConnect platform. Click on it to start the process</li>
+                <li style={{listStyle: 'disc'}}>Provide all the necessary information about your load.</li>
+                <li style={{listStyle: 'disc'}}>Determine the bidding options for the load. You can choose to set a specific price range and submit</li>
+                <li style={{listStyle: 'disc'}}>Drivers on LoadConnect will see your load posting and have the opportunity to submit bids with their proposed prices</li>
+                <li style={{listStyle: 'disc'}}> Evaluate the bids received based on factors such as price, driver ratings, equipment, and any other criteria that are important to you</li>
+                <li style={{listStyle: 'disc'}}> Once you have selected a driver, communicate the final details, such as pickup and delivery schedules, payment terms, and any necessary documentation.</li>
             </ul>
         </div>
       </div>
