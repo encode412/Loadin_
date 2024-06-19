b6bdfd533061294bfedebd52a25220437bd2e1c5
encodedevv <timothyonyea@gmail.com>
2024-06-19 09:20:46 +0100

    replacement of dummy data

--- src/pages/dashboard/_components/load-matching/_components/feature-modal/index.jsx
+++ src/pages/dashboard/_components/load-matching/_components/feature-modal/index.jsx
@@ -4,16 +4,32 @@ const FeatureModal = () => {
   return (
     <div className="bg-white rounded-[20px] w-full">
       <div className="flex px-6 py-8 flex-col">
-        <span className="text-primary text-3xl font-bold">How to use LoadMatching</span>
+        <span className="text-primary text-3xl font-bold">
+          How to use LoadMatching
+        </span>
         <div className="flex flex-col px-6 pt-6">
-            <ul className="text-base flex flex-col gap-y-3 font-medium">
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>loremAccess to best pricesAccess to best pricesAccess to best pricesAccess to best prices</li>
-                <li style={{listStyle: 'disc'}}>Access to best prices</li>
-                <li style={{listStyle: 'disc'}}>Access to best prices</li>
-            </ul>
+          <ul className="text-base flex flex-col gap-y-3 font-medium">
+            <li style={{ listStyle: "disc" }}>
+              Provide detailed information about your load, including origin,
+              destination, load type, weight, and any specific requirements or
+              preferences. Posting your load accurately and comprehensively
+              increases the chances of the system matching you to the right
+              driver
+            </li>
+            <li style={{ listStyle: "disc" }}>
+              Drivers can access the LoadMatching feature and use search filters
+              to find suitable loads. Filters may include origin, destination,
+              load type, weight range, and other criteria
+            </li>
+            <li style={{ listStyle: "disc" }}>
+              {" "}
+              After reaching an agreement, the shipper and carrier can finalize
+              the load transportation arrangement. This includes confirming the
+              pick-up and delivery details, establishing the payment terms, and
+              ensuring all necessary documentation is in order.
+            </li>
+           
+          </ul>
         </div>
       </div>
     </div>
--- src/pages/dashboard/_components/load-matching/_components/hero-section/index.jsx
+++ src/pages/dashboard/_components/load-matching/_components/hero-section/index.jsx
@@ -11,22 +11,22 @@ const HeroSection = () => {
         <div className="bg-black bg-opacity-40 top-0 w-full absolute left-0 right-0 h-full lg:h-[40rem] z-10" />
         <main className="relative">
           <div className="overflow-hidden bg-right bg-no-repeat bg-cover h-screen lg:h-[40rem] relative bg-postloadbg">
-            <div className="flex flex-col px-8 md:px-10 max-w-[1184px] pt-[10rem] md:pt-40 mx-auto space-y-4 animate-slide_up ">
+            <div className="flex flex-col px-8 md:px-10 max-w-[1184px] pt-[6rem] md:pt-40 mx-auto space-y-4 animate-slide_up ">
               <span className="text-4xl uppecase lg:text-5xl lg:!leading-[5.2rem] xl:text-7xl font-bold text-white z-10">
                 What is
                 <br />
                 <span className="uppercae">LoadMatching</span>
               </span>
               <span className="font-medium text-[#eee] text-lg md:text-xl w-full md:w-1/2 z-10">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
-                deleniti ad nesciunt consequatur dicta nisi doloribus ipsa
-                dolores quae? Distinctio, voluptatum totam aperiam excepturi
-                accusantium repellat molestiae perspiciatis eveniet ipsam.
+              Our intelligent load matching feature takes the guesswork out of choosing the right vehicle for your transport needs. By analyzing your load's characteristics, our system suggests the most suitable vehicle, ensuring safe and efficient transportation. Experience a seamless and hassle-free process with optimized vehicle selection.
               </span>
-            </div>
-            <div className="z-20 shadow-2xl lg:right-[10%] top-[50%] rounded-[20px] lg:w-[30%] h-full absolute bg-white">
+              <div className="z-20 lg:hidden block shadow-2xl lg:right-[10%] top-[50%] rounded-[20px] lg:w-[30%] h-full absolute bg-white">
               <FeatureModal />
             </div>
+          </div>{" "}
+          <div className="z-20 lg:block hidden shadow-2xl lg:right-[10%] top-[30%] rounded-[20px] lg:w-[30%] h-full absolute bg-white">
+            <FeatureModal />
+          </div>
           </div>
           <div className="absolute z-[999] left-[40%] top-[50%]"></div>
         </main>
