e6b279ca2311ce51592ff6665d8a3f56c4244f3c
encodedevv <timothyonyea@gmail.com>
2024-06-19 06:14:37 +0100

    update

--- src/pages/dashboard/_components/loadwallet/_components/services-card/index.jsx
+++ src/pages/dashboard/_components/loadwallet/_components/services-card/index.jsx
@@ -9,9 +9,10 @@ const ServicesCard = () => {
             Benefits of using LoadWallet.
           </span>
           <span className="text-lightgray lg:w-1/2 lg:text-lg">
-            Consectetur optio perferendis dignissimos animi dolor, sapiente,
-            molestiae voluptates corrupti harum fugit quam consequuntur
-            molestiae voluptates corrupti harum fugit quam consequuntur
+          LoadWallet offers a convenient payment solution within the LoadConnect platform. Users can seamlessly request and approve payments with just a few clicks, eliminating the need for traditional transfer methods and streamlining the payment process. <br /> <br />
+          </span>
+          <span className="text-lightgray lg:w-1/2 lg:text-lg">
+          By using LoadWallet, both drivers and customers can save valuable time. Drivers can easily request payment directly on the platform, while customers can quickly review and approve the payment requests. This removes the need for manual payment requests and reduces administrative tasks, resulting in more efficient transactions.
           </span>
 
         
